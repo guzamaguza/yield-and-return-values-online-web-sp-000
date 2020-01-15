663cc44174c75a841ec47d3673a218a1a2654848
Mark A Guza <guzamaguza@gmail.com>
2020-01-15 05:46:36 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+=begin
 def hello(array)
   i = 0
   while i < array.length
@@ -8,6 +9,19 @@ def hello(array)
     i += 1
   end
 end
+=end
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+
 
 #binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
